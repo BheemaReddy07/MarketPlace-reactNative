714400866fd55ff70e4eec548f65482c97055827
BheemaReddy07 <bheemareddy2910@gmail.com>
2025-07-04 20:37:52 +0530

    Navigation setup

---
+++ screens/add/AddNewProductScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddNewProductScreen = () => {
+  return (
+    <View>
+      <Text>AddNewProductScreen</Text>
+    </View>
+  )
+}
+
+export default AddNewProductScreen
+
+const styles = StyleSheet.create({})
